8eb45190099c1f252edea36bb53bcb93596f4c2f
vheimpel <vheimpel@gmail.com>
2017-05-13 19:17:49 +0000

    Encrypts passwords and cookies

--- express_server.js
+++ express_server.js
@@ -10,20 +10,62 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs")
 
-var shortURL = "";
-var longURL = "";
+const bcrypt = require('bcrypt');
+
+var cookieSession = require('cookie-session')
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ["iac"],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 const users = {
-  "example": {
-    id: "example",
+  "hello": {
+    id: "hello",
     email: "hello@hello.com",
     password: "hey"
+  },
+  "bye": {
+    id: "bye",
+    email: "bye@bye.com",
+    password: "bye"
   }
 }
 
+// var urlDatabase = {
+//   "hello": {
+//     "b2xVn2": "http://www.lighthouselabs.ca",
+//     "9sm5xK": "http://www.google.com",
+//   },
+//   "bye": {
+//     "c3yWm3": "http://www.hackeryou.ca",
+//     "0tn6yL": "http://www.askjeeves.com",
+//   }
+// };
+
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "hello",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "hello"
+  },
+
+  "c3yWm3": {
+     longURL: "http://www.hackeryou.ca",
+     userID: "bye"
+  },
+
+  "0tn6yL": {
+    longURL: "http://www.askjeeves.com",
+    userID: "bye"
+  }
+
 };
 
 function generateRandomString() {
@@ -50,21 +92,32 @@ function getUserById(id) {
   }
 }
 
+function getURLid(id) {
+  var urlDatabaseFiltered = {}
+  for (var i in urlDatabase) {
+    if (urlDatabase[i].userID === id) {
+      urlDatabaseFiltered[i] = urlDatabase[i];
+    }
+  }
+  return urlDatabaseFiltered;
+}
+
 app.get("/", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+  let wholeUser = getUserById(req.session.user_id);
   let templateVars = { user: wholeUser}
   res.render("urls_new", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+  let wholeUser = getUserById(req.session.user_id);
   let templateVars = { user: wholeUser}
   console.log(wholeUser)
   res.render("login", templateVars)
 })
 
 app.get("/register", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+
+  let wholeUser = getUserById(req.session.user_id);
   let templateVars = { user: wholeUser}
   res.render("register", templateVars)
 })
@@ -72,6 +125,8 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const userEmail = req.body.email;
   const userPassword = req.body.password;
+  const hashedPassword = bcrypt.hashSync(userPassword, 10);
+
   if (userEmail === "" || userPassword === "") {
     res.status(400)
     res.send("Please enter all information")
@@ -89,12 +144,12 @@ app.post("/register", (req, res) => {
   const user = {
     id: userId,
     email: userEmail,
-    password: userPassword
+    password: hashedPassword
   }
   users[userId] = user;
   console.log("Users object: ", users)
 
-  res.cookie('user_id', users[userId].id);
+  req.session.user_id = users[userId].id;
   res.redirect('/urls/new');
 });
 
@@ -102,6 +157,7 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
   const userEmail = req.body.userEmail;
   const userPassword = req.body.userPassword;
+
   console.log(req.body)
   if (userEmail === "" || typeof(userEmail) === "undefined"  || userPassword === "" || typeof(userPassword) === "undefined") {
     res.status(400)
@@ -110,10 +166,11 @@ app.post("/login", (req, res) => {
 
   for (var i in users) {
     if (users[i].email === userEmail) {
-      if (users[i].password === userPassword) {
-        res.cookie('user_id', users[i].id);
+      if (bcrypt.compareSync(userPassword, users[i].password)) {
+        req.session.user_id = users[i].id;
+        console.log(users)
         res.redirect("/urls/new");
-        console.log("at line 110")
+
       }
     }
   }
@@ -122,9 +179,8 @@ app.post("/login", (req, res) => {
 
 });
 
-
 app.get("/urls/new", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+  let wholeUser = getUserById(req.session.user_id);
   let templateVars = { user: wholeUser}
   res.render("urls_new", templateVars);
 });
@@ -134,18 +190,16 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+  let wholeUser = getUserById(req.session.user_id);
   console.log("WholeUser :", wholeUser);
-  let templateVars = { urls: urlDatabase, user: wholeUser };
-  console.log("templateVars: ", templateVars)
+  let templateVars = { urls: getURLid(req.session.user_id), user: wholeUser };
+  console.log("getUserById: ", getUserById(req.session.user_id))
+
   res.render("urls_index", templateVars);
-   // let templateVars = {
-   //        userInfo: users[i]
-   //      }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+  let wholeUser = getUserById(req.session.user_id);
   let templateVars = { user: wholeUser}
   let longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL, templateVars);
@@ -156,30 +210,47 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 })
 
+//Create urls
 app.post("/urls", (req, res) => {
   let newShortUrl = generateRandomString();
-  urlDatabase[newShortUrl] = req.body.longURL;
+  urlDatabase[newShortUrl] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id
+  }
+  console.log(urlDatabase)
   res.redirect("/urls/" + newShortUrl);
 });
 
+//Edit endpoint
 app.get("/urls/:id", (req, res) => {
-  let wholeUser = getUserById(req.cookies.user_id);
+  let wholeUser = getUserById(req.session.user_id);
   let templateVars = {
     newShortURL: req.params.id,
-    longURL: urlDatabase[req.params.id],
-    user: wholeUser
+    longURL: urlDatabase[req.params.id].longURL,
+    user: wholeUser,
+    urls: urlDatabase
   };
+  console.log(urlDatabase[req.params.id])
+  const userID = req.session.user_id;
+
+  if (!userID) return res.redirect("/");
+
   res.render("urls_show", templateVars);
+
 });
 
+//Delete endpoint
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
-
+//Update endpoint
 app.post('/urls/:shortURL', (req, res) => {
   const id = req.params.shortURL;
-  urlDatabase[id] = req.body.updateField;
+  urlDatabase[id] = {
+    longURL: req.body.updateField,
+    userID: req.session.user_id
+  }
 
   res.redirect('/urls')
 });
